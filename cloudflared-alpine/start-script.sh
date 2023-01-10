6f86a8c3c482b7bb31d0a55ef77f48aca375484e
KoomeKiriinya <ian@cloudkite.io>
2023-01-11 00:02:26 +0300

    add infinite sleep to keep container running

--- cloudflared-alpine/start-script.sh
+++ cloudflared-alpine/start-script.sh
@@ -6,3 +6,4 @@ do
    cloudflared access tcp --id $CF_SERVICE_AUTH_ID --secret $CF_SERVICE_AUTH_SECRET  --hostname $instance --url "tcp://127.0.0.1:$(expr $BASE_PORT + $ITER)"&
    ITER=$(expr $ITER + 1)
 done
+sleep infinity
