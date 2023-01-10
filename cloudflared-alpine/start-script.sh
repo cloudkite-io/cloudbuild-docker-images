68d2a34e0370af1567d836df7782866c504a73de
KoomeKiriinya <ian@cloudkite.io>
2023-01-10 22:00:23 +0300

    set correct service

--- cloudflared-alpine/start-script.sh
+++ cloudflared-alpine/start-script.sh
@@ -3,5 +3,5 @@ IFS=","
 ITER=0
 for instance in $INSTANCES
 do
-   cloudflared access tcp --id $CF_SERVICE_AUTH_ID --secret $CF_SERVICE_AUTH_SECRET  --hostname $instance --url "http://127.0.0.1:$(expr $BASE_PORT + $ITER)"&
+   cloudflared access tcp --id $CF_SERVICE_AUTH_ID --secret $CF_SERVICE_AUTH_SECRET  --hostname $instance --url "tcp://127.0.0.1:$(expr $BASE_PORT + $ITER)"&
 done
