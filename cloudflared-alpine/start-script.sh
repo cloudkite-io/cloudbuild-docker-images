bb8768e488be9732285968cb7bb4ccb515d08721
KoomeKiriinya <ian@cloudkite.io>
2023-01-10 22:02:27 +0300

    set iter value

--- cloudflared-alpine/start-script.sh
+++ cloudflared-alpine/start-script.sh
@@ -4,4 +4,5 @@ ITER=0
 for instance in $INSTANCES
 do
    cloudflared access tcp --id $CF_SERVICE_AUTH_ID --secret $CF_SERVICE_AUTH_SECRET  --hostname $instance --url "tcp://127.0.0.1:$(expr $BASE_PORT + $ITER)"&
+   ITER=$(expr $ITER + 1)
 done
