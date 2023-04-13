6ef8b6d4c852d1ac2fc32c5319aaa9fc40a738fe
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2023-04-13 18:29:27 -0500

    [cloudflared-alpine] Allow for tunnel address to be configured, and if not set default to `127.0.0.1`

--- cloudflared-alpine/start-script.sh
+++ cloudflared-alpine/start-script.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 IFS=","
 ITER=0
-for instance in $INSTANCES
+for instance in ${INSTANCES}
 do
-   cloudflared access tcp --id $CF_SERVICE_AUTH_ID --secret $CF_SERVICE_AUTH_SECRET  --hostname $instance --url "tcp://127.0.0.1:$(expr $BASE_PORT + $ITER)"&
-   ITER=$(expr $ITER + 1)
+   cloudflared access tcp --id "${CF_SERVICE_AUTH_ID}" --secret "${CF_SERVICE_AUTH_SECRET}"  --hostname "${instance}" --url "tcp://${TUNNEL_ADDRESS:-127.0.0.1}:$(expr ${BASE_PORT} + ${ITER})"&
+   ITER=$(expr ${ITER} + 1)
 done
 sleep infinity
